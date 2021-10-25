af10f75cc041dc9e87fffba6c1d6971d312fed8e
Grimeysz <32318053+Grimeysz@users.noreply.github.com>
2021-10-25 10:24:28 -0400

    product page customization

---
+++ js/script.js
@@ -0,0 +1,34 @@
+var nItems = 0;
+
+document.getElementById("addtocart").onclick = function addItem() {
+  nItems = nItems + 1;
+  document.getElementById("cartitems").innerHTML = nItems;
+  document.getElementById("popup").style.display = "block";
+  setTimeout(() => {
+    function x() {
+      document.getElementById("popup").style.display = "hidden";
+    }
+  }, 1000);
+};
+
+function changeBackground(image) {
+  document.getElementById("floor-pouf").style.backgroundImage = image;
+  console.log("image changed");
+}
+const a = document.getElementsByClassName("color-options");
+
+a[0].onclick = function x() {
+  changeBackground("url(shop-items/floor-pouf4.jpg)");
+};
+
+a[1].onclick = function x() {
+  changeBackground("url(shop-items/floor-pouf2.jpg)");
+};
+
+a[2].onclick = function x() {
+  changeBackground("url(shop-items/floor-pouf3.jpg)");
+};
+
+a[3].onclick = function x() {
+  changeBackground("url(shop-items/floor-pouf.jpg)");
+};
